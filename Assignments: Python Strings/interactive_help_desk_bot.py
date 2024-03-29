8bf55a3170fb00d943a9f195e252ead79f328a32
KarenJas <kjgonzalez445@gmail.com>
2024-03-29 08:12:28 -0500

    final update

---
+++ Assignments: Python Strings/interactive_help_desk_bot.py
@@ -0,0 +1,25 @@
+'''3. Interactive Help Desk Bot'''
+
+text_input = input("How can I best help you today? : ")
+user_data = []
+
+if "help" in text_input:
+    help = input("what do you need help with? : ")
+    user_data.append(help)
+elif "issue" in text_input:
+    issue = input('What seems to be the issue? : ')
+    user_data.append(issue)
+    if "login" in issue:
+        print("This is a login issue ")
+    elif "performance" in issue:
+        print("This is a performance issue")
+    elif "error" in issue:
+        print("This is a error issue ")
+elif "contact support" in text_input:
+    contact_support = input("What department would you like to contact (a,b,c)? : ")
+    user_data.append(contact_support)
+
+
+
+
+
